37a42f9823d5675e590bcaf7d87efc39aef99d5e
HanGyeongjun <030212hgjun@gmail.com>
2023-11-05 18:54:43 +0900

    [Init] Create Xcode Project
    - Language : Swift
    - Interface : SwiftUI
    - Storage : None
    - Include Tests

---
+++ HalogenAppTemplate/HalogenAppTemplate/HalogenAppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalogenAppTemplateApp.swift
+//  HalogenAppTemplate
+//
+//  Created by HanGyeongjun on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct HalogenAppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
